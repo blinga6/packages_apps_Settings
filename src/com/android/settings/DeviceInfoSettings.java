4929339dcd15a20a81c0847a062f0d13cf1b0cd3
Danesh Mondegarian <daneshm90@gmail.com>
2012-12-09 16:59:15 -0500

    Turbo PlatLogo: Turbo ROM version preference (2/2)
    Binds the Turbo version preference to launch Turbo easter egg
    upon multiple taps.
    
    * Original commit by CM, modified for Turbo
    
    Change-Id: I8e06642944d24581c0932c9964f2d9615bfcb832

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -304,6 +304,20 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
 		    supported = (getPackageManager().getPackageInfo("com.turborom.turboota", 0).versionCode > 0);
 		} catch (PackageManager.NameNotFoundException e) {
 	    }
+        } else if (preference.getKey().equals(KEY_TURBO_VERSION)) {
+            System.arraycopy(mHits, 1, mHits, 0, mHits.length-1);
+            mHits[mHits.length-1] = SystemClock.uptimeMillis();
+            if (mHits[0] >= (SystemClock.uptimeMillis()-500)) {
+                Intent intent = new Intent(Intent.ACTION_MAIN);
+                intent.putExtra("is_turbo", true);
+                intent.setClassName("android",
+                        com.android.internal.app.PlatLogoActivity.class.getName());
+                try {
+                    startActivity(intent);
+                } catch (Exception e) {
+                    Log.e(LOG_TAG, "Unable to start activity " + intent.toString());
+                }
+            }
         }
         return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
